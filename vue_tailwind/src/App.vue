0183a00cef0963ec173d85d638a42a244c02535d
Riccardo Colombo <rikyregna@gmail.com>
2024-07-03 23:31:10 +0200

    init

---
+++ vue_tailwind/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+	<header>
+		<RouterLink to="/">
+			<span :class="'bg-white'">Home</span>
+		</RouterLink>
+		<RouterLink to="/about">About</RouterLink>
+	</header>
+
+	<RouterView />
+</template>
